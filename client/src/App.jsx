fdd5a37b3aa311eb85e46efae83773b1e26e0896
Nitin Varghese <nitin@pix.city>
2023-10-06 19:54:41 +0530

    Added sign up logic, added useQuery and useForm

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,20 +6,24 @@ import SignIn from "./pages/SignIn.jsx";
 import SignUp from "./pages/SignUp.jsx";
 import About from "./pages/About.jsx";
 import Header from "./components/Header.jsx";
+import {useQuery, useMutation, useQueryClient, QueryClient, QueryClientProvider,} from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
 export default function App() {
   return (
-    <BrowserRouter>
-        <Header />
-        <Routes>
-            <Route path={'/'} element={<Home />} />
-            <Route path={'/sign-in'} element={<SignIn />} />
-            <Route path={'/sign-up'} element={<SignUp />} />
-            <Route path={'/about'} element={<About />} />
-            <Route path={'/profile'} element={<Profile />} />
-        </Routes>
+      <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+              <Header />
+              <Routes>
+                  <Route path={'/'} element={<Home />} />
+                  <Route path={'/sign-in'} element={<SignIn />} />
+                  <Route path={'/sign-up'} element={<SignUp />} />
+                  <Route path={'/about'} element={<About />} />
+                  <Route path={'/profile'} element={<Profile />} />
+              </Routes>
 
 
-    </BrowserRouter>
+          </BrowserRouter>
+      </QueryClientProvider>
   )
 }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
-import {Link} from "react-router-dom";
+import React, {useState} from 'react'
+import {Link, useNavigate} from "react-router-dom";
+import {useQuery,
+    useMutation,
+    useQueryClient,
+    QueryClientProvider,} from "@tanstack/react-query";
+import {useForm} from "react-hook-form";
+import axios from 'axios';
 
 export default function SignUp() {
+    const queryClient = useQueryClient();
+    const navigate = useNavigate();
+    const signup = (data) => {
+        return axios.post(`/api/auth/signup`, data).then((res) => {
+            setTimeout(() => {
+                navigate('/sign-in');
+            }, 2000);
+            return res;
+        }).catch((err) => {
+            return(err.response);
+        });
+    }
+
+    const {mutate, isLoading, error, data} = useMutation({
+        mutationFn: (formData) => { return signup(formData) },
+        onSuccess: () => {
+            // Invalidate and refetch
+            queryClient.invalidateQueries({ queryKey: ['signup'] });
+        },
+    });
+
+    console.log(error, data);
+    const { handleSubmit, register, formState: { errors } } = useForm({});
+
+    const onSubmit = (data) => {
+        console.log(data);
+        // mutateAsync().then(res => {
+        //     if(res.data.success){
+                mutate(data);
+        //     }
+        // });
+    };
+
     return (
         <>
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -17,66 +56,91 @@ export default function SignUp() {
                 </div>
 
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                    <form className="space-y-6" action="#" method="POST">
-                        <div>
-                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                                Username
-                            </label>
-                            <div className="mt-2">
-                                <input
-                                    id="username"
-                                    name="username"
-                                    type="text"
-                                    required
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                            </div>
-                        </div>
-
-                        <div>
-                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                                Email address
-                            </label>
-                            <div className="mt-2">
-                                <input
-                                    id="email"
-                                    name="email"
-                                    type="email"
-                                    autoComplete="email"
-                                    required
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                            </div>
-                        </div>
-
-                        <div>
-                            <div className="flex items-center justify-between">
-                                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                                    Password
+                    <QueryClientProvider client={queryClient}>
+                        <form className="space-y-6" action="#" method="POST" onSubmit={handleSubmit(onSubmit)}>
+                            <div>
+                                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Username
                                 </label>
+                                <div className="mt-2">
+                                    <input
+                                        {...register("username", {
+                                            required: true,
+                                            maxLength: 200,
+                                        })}
+                                        type="text"
+                                        required
+                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    />
+                                </div>
                             </div>
-                            <div className="mt-2">
-                                <input
-                                    id="password"
-                                    name="password"
-                                    type="password"
-                                    autoComplete="current-password"
-                                    required
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
+
+                            <div>
+                                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Email address
+                                </label>
+                                <div className="mt-2">
+                                    <input
+                                        {...register("email")}
+                                        type="email"
+                                        autoComplete="email"
+                                        required
+                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    />
+                                </div>
                             </div>
-                        </div>
 
-                        <div>
-                            <button
-                                type="submit"
-                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                            >
-                                Sign Up
-                            </button>
-                        </div>
-                    </form>
+                            <div>
+                                <div className="flex items-center justify-between">
+                                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                                        Password
+                                    </label>
+                                </div>
+                                <div className="mt-2">
+                                    <input
+                                        {...register("password")}
+                                        type="password"
+                                        autoComplete="current-password"
+                                        required
+                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    />
+                                </div>
+                            </div>
 
+                            <div>
+                                <button
+                                    type="submit"
+                                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                >
+                                    {isLoading ? (
+                                        <div
+                                            className="inline-block h-6 w-6 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                                            role="status">
+                                              <span
+                                                  className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+                                              >Loading...</span>
+                                        </div>
+                                    ) : 'Sign Up'}
+                                </button>
+                            </div>
+                        </form>
+
+                    </QueryClientProvider>
+
+                    <div className={'py-3 alert'}>
+                        {data?.data?.statusCode === 500 && (
+                            <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                                <p className={'text-red-500 text-xs'}>{data?.data?.message}</p>
+                            </div>
+                        )}
+
+                        {data?.status === 201 && (
+                            <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+                                <p className={'text-green-700 text-xs'}>{data?.data} {'You will be redirected to login page'}</p>
+                            </div>
+                        )}
+
+                    </div>
                     <p className="mt-10 text-center text-sm text-gray-500">
                         Already a member?{' '}
                         <Link to={"/sign-in"} className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
