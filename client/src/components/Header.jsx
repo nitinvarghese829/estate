87540f2d9e78878e2eeb05e8da0a39fb50962bce
Nitin Varghese <nitin@pix.city>
2023-10-06 16:48:03 +0530

    added sign up ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
 
                 <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
                     <input type='text' placeholder='
-                    search...' className='bg-transparent focus:outline-none w-24 sm:w-64' />
+                    search...' className='bg-transparent focus:outline-none focus:border-none w-24 sm:w-64  outline-none border-none' />
                     <FaSearch className='text-slate-500' />
                 </form>
 
