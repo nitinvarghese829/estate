21aaed446d358e4bb79267e4b5db55043fdd11e3
Nitin Varghese <nitin@pix.city>
2023-10-07 11:22:00 +0530

    sign in

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcyprtjs from 'bcryptjs'
 import {errorHandler} from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
@@ -10,6 +11,32 @@ export const signup = async (req, res, next) => {
     try{
         await newUser.save();
         res.status(201).json('User created successfullyyy');
+    } catch (e) {
+        next(errorHandler(e.statusCode, e.message));
+    }
+}
+
+export const signIn = async (req, res, next) => {
+    const {email, password} = req.body;
+
+
+
+    try{
+        const validUser = await User.findOne({email});
+
+        if(!validUser){
+            next(errorHandler(404, 'User not found'));
+        }
+
+        const validPassword = bcyprtjs.compareSync(password, validUser.password);
+        if(!validPassword) {
+            return next(errorHandler(401, 'Invalid Credentials'));
+        }
+
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+
+        const {password: pwd,  ...rest} = validUser._doc;
+        res.cookie('access_token', token, {httpOnly: true}).status(200).json(rest);
     } catch (e) {
         next(errorHandler(e.statusCode, e.message));
     }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -51,7 +51,7 @@ export default function SignUp() {
                         alt="Your Company"
                     />
                     <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                        Sign in to your account
+                        Sign up with us
                     </h2>
                 </div>
 
