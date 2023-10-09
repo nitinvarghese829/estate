876306510a9400a23d8ef828dde1251cb3f14448
Nitin Varghese <nitin@pix.city>
2023-10-09 13:54:11 +0530

    update profile api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,31 @@
+import {errorHandler} from "../utils/error.js";
+import User from "../models/user.model.js";
+
 export const test = (req, res) => {
     res.json({message: "Api route Hello world"});
+}
+
+export const updateUser = async (req, res, next) => {
+    if(req.user.id !== req.params.id) return next(errorHandler(401, 'You can update oly your profile'));
+    try{
+        if(req.body.password){
+            req.body.password = bcrypt.hashSync(req.body.password, 10);
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set:{
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                avatar: req.body.avatar,
+            }
+        }, {new: true});
+
+        const {password, ...rest} = updatedUser._doc;
+
+        res.status(200).json(rest);
+    } catch (e) {
+        next(errorHandler())
+    }
+
 }
